7a7a0e5f6bc266629f7885d0e6c4b725bb2ddaae
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-01-25 07:25:08 -0500

    error en el ordenamiento

--- 5.ordenamiento.py
+++ 5.ordenamiento.py
@@ -1,7 +1,7 @@
 cantidad = int(input("Ingrese la cantidad de numeros: \n"))
 numero = []
 for i in range(cantidad):
-    numero.append(input(f"Ingrese el numero: {i + 1}: \n"))
+    numero.append(int(input(f"Ingrese el numero: {i + 1}: \n")))
 
 numero_ordenado = sorted(numero) #sorted sirve para ordenar una lista
 
