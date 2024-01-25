f738f03f0176d7c8f17b30c863e9dcbcf96d6da0
156461877+Camilo-fd@users.noreply.github.com <Camilo-fd>
2024-01-24 21:02:05 -0500

    correcion en el codigo

--- 4.palabraMasLarga.py
+++ 4.palabraMasLarga.py
@@ -1,8 +1,8 @@
 palabra1 = input("Escribe la palabra: ")
 palabra2 = input("Escribe la palabra: ")
 
-largopalabra1 = palabra1
-largopalabra2 = palabra2
+largopalabra1 = len(palabra1)
+largopalabra2 = len(palabra2)
 
 if largopalabra1 > largopalabra2:
     print(f"La palabra {palabra1} es mas larga que la palabra {palabra2}")
